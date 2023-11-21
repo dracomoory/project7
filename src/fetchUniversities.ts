5b027b1fa96858e45b9e7fca47ce3075e446ccd9
vc <zhang@umass.edu>
2023-11-21 03:44:02 -0500

    update 11/21

--- src/fetchUniversities.ts
+++ src/fetchUniversities.ts
@@ -9,13 +9,13 @@ export function fetchUniversities(query: string): Promise<string[]> {
     **/
     query = "http://220.maxkuechen.com/universities/search?name=".concat(query);
     return fetch(query)
-        .then((result: Response): object => (result.ok ? result.json() : {}))
+        .then((result: Response): object => (result.ok ? result.json() as object : {} as object))
         .then((jsonObj: object): string[] => {
             if (!Array.isArray(jsonObj)) return [] as string[];
             // now jsonObj is of object[]
             return jsonObj.reduce((acc: string[], elem: object): string[] => {
                 if ("name" in elem) acc.push("".concat(elem["name"] as string));
                 return acc;
-            }, [] as string[]);
+            }, [] as string[]) as string[];
         });
 }
