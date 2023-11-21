519dbe6983880a0c4b830a8bfbd96e248dd80ae3
vc <zhang@umass.edu>
2023-11-21 00:20:21 -0500

    update 11/21

--- src/universityWeather.ts
+++ src/universityWeather.ts
@@ -1,9 +1,10 @@
-import { fetchUniversities } from "./fetchUniversities.js";
-import { fetchGeoCoord, GeoCoord } from "./fetchGeoCoord.js";
-import { fetchCurrentTemperature, TemperatureReading } from "./fetchCurrentTemperature.js";
+import {fetchUniversities} from "./fetchUniversities.js";
+import {fetchGeoCoord, GeoCoord} from "./fetchGeoCoord.js";
+import {fetchCurrentTemperature, TemperatureReading} from "./fetchCurrentTemperature.js";
 
 interface AverageTemperatureResults {
     totalAverage: number;
+
     [key: string]: number;
 }
 
@@ -12,30 +13,37 @@ export function fetchUniversityWeather(
     transformName?: (s: string) => string
 ): Promise<AverageTemperatureResults> {
     // TODO
-
-    const obj: AverageTemperatureResults = { totalAverage: 0 };
+    const obj: AverageTemperatureResults = {totalAverage: 0};
     let total = 0;
     let count = 0;
 
-    return fetchUniversities(universityQuery).then((x: string[]) => {
-        x.forEach((y: string) =>
-            (transformName !== undefined ? fetchGeoCoord(transformName(y)) : fetchGeoCoord(y)).then((z: GeoCoord) =>
-                fetchCurrentTemperature(z).then((t: TemperatureReading) => {
-                    obj[y] = t.temperature_2m.reduce((s, a) => s + a, 0) / t.temperature_2m.length;
-                    total += obj[y];
-                    ++count;
-                })
-            )
-        );
-        obj.totalAverage = total / count;
-        return obj;
-    });
+    return fetchUniversities(universityQuery)
+        .then((x: string[]) => {
+            return x.map((y: string) =>
+                (transformName !== undefined ? fetchGeoCoord(transformName(y)) : fetchGeoCoord(y)).then((z: GeoCoord) =>
+                    fetchCurrentTemperature(z).then((t: TemperatureReading) => {
+                        obj[y] = t.temperature_2m.reduce((s, a) => s + a, 0) / t.temperature_2m.length;
+                        total += obj[y];
+                        ++count;
+
+                        if (count === x.length) {
+                            obj.totalAverage = total / count;
+                        }
+                        return obj;
+                    })
+                )
+            );
+        })
+        .then(arr => Promise.all(arr).then(x => x[0]));
 }
 
 export function fetchUMassWeather(): Promise<AverageTemperatureResults> {
     // TODO
     return fetchUniversityWeather("University of Massachusetts", x => {
-        return x === "University of Massachussetts at Amherst" ? "University of Massachussetts Amherst" : x;
+        if (x === "University of Massachusetts at Amherst") return "University of Massachusetts Amherst";
+        if (x === "University of Massachusetts at Lowell") return "University of Massachusetts Lowell";
+        if (x === "University of Massachusetts at Dartmouth") return "University of Massachusetts Dartmouth";
+        return x;
     });
 }
 
